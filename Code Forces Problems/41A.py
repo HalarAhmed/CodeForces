79958903b154ec125562b91ebfa09eb0bc0ff107
HalarAhmed <work.halar@gmail.com>
2025-08-03 23:17:41 +0500

    41A-Task
    Link:
    https://codeforces.com/problemset/problem/41/A

---
+++ Code Forces Problems/41A.py
@@ -0,0 +1,7 @@
+berland=input()
+birland=input()
+if len(berland)<=100 and len(birland)<=100 and len(berland)!=0 and len(birland)!=0:
+    if berland[::-1]==birland:
+        print("YES")
+    else:
+        print("NO")
