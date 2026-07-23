63e4ab7bb2054189ceb5a25f10b4ebb9833ebc8b
Halar Ahmed <u2024194@giki.edu.pk>
2026-07-23 14:33:29 +0500

    1828B - Permutation Swap
    Link:
    https://codeforces.com/problemset/problem/1828/B

---
+++ Code Forces Problems/1828B.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	long long t,n,k;
+	cin>>t;
+	while(t--)
+	{
+        cin>>n;
+        vector<long long>p(n);
+        for(long long i=0;i<n;++i)  
+        {
+            cin>>p[i];
+        }
+        k=abs(1-p[0]);
+        for(int i=1;i<n;++i)
+        {
+            k=gcd(abs((i+1)-p[i]),k);
+        }
+        cout<<k<<endl;
+	}
+}
